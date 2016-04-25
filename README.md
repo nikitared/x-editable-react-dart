0d665f8edb2c76fe52829d8852f200268f0494cc
babaninnv <nikitared@iwlab.net>
2016-04-26 03:46:30 +0700

    added: font-awesome, x-editable icons. Updated states of text edit component

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
-###_x-editable_ components wrapped _react-dart_
+## Introduction
+
+_x-editable_ components wrapped _react-dart_
+
+## Libraries using x_editatble_react_dart
 
 * **X-editable** - https://vitalets.github.io/x-editable - in-place editing with Twitter Bootstrap, jQuery UI or pure jQuery
 * **React-Dart** - https://github.com/cleandart/react-dart - Dart wrapper library for facebook/react
+
+## Getting Started
+
+## Components list
+
+* Simple text field
+
+## Examples
--- lib/components/simple_text_field.dart
+++ lib/components/simple_text_field.dart
@@ -1,22 +1,26 @@
 import "package:react/react.dart";
+import "package:react/react_dom.dart" as dom;
 import 'package:x_editable_react_dart/components/x_editable.dart';
+import 'dart:html';
 
 class SimpleTextField extends Component implements XEditable {
 
   Map getInitialState() => {
-    "editable": false
+    "editable": false,
+    "value": "",
+    "tmp": ""
   };
 
   toEdit(event) {
-    setState({"editable": true});
+    setState({"editable": true, "tmp": state["value"]});
   }
 
   clear(event) {
-
+    setState({"tmp": ""});
   }
 
   apply(event) {
-    setState({"editable": false});
+    setState({"editable": false, "value": state["tmp"]});
   }
 
   cancel(event) {
@@ -41,15 +45,33 @@ class SimpleTextField extends Component implements XEditable {
               form({"className": "form-inline editableform"},
                   div({"className": "control-group form-group"},
                       div({},
-                          div({"className": "editable-input", "style": {"position": "relative"}},
-                              input({"type": "text", "className": "form-control input-sm", "style": {"paddingRight": "24px"}}),
-                              span({"className": "editable-clear-x", "onClick": clear})
+                          div({
+                            "className": "editable-input",
+                            "style": {"position": "relative"}
+                          },
+                              input({
+                                "type": "text",
+                                "value": bind("tmp"),
+                                "className": "form-control input-sm",
+                                "style": {"paddingRight": "24px"}
+                              }),
+                              span({
+                                "className": "editable-clear-x",
+                                "onClick": clear
+                              })
                           ),
                           div({"className": "editable-buttons"},
-                              button({"type": "submit", "onClick": apply, "className": "btn btn-primary btn-sm editable-submit"},
+                              button({
+                                "type": "submit",
+                                "onClick": apply,
+                                "className": "btn btn-primary btn-sm editable-submit"
+                              },
                                   i({"className": "fa fa-check"})
                               ),
-                              button({"type": "button", "onClick": cancel, "className": "btn btn-default btn-sm editable-cancel"
+                              button({
+                                "type": "button",
+                                "onClick": cancel,
+                                "className": "btn btn-default btn-sm editable-cancel"
                               },
                                   i({"className": "fa fa-remove"})
                               )
@@ -67,15 +89,13 @@ class SimpleTextField extends Component implements XEditable {
 
   @override
   view() {
-    return a({"href": "#",
-      "onClick": toEdit,
-      "id": "username",
+    return a({"href": "#", "onClick": toEdit, "id": "username",
       "data-type": "text",
       "data-pk": "1",
       "data-title": "Enter username",
       "className": "editable editable-click",
       "style": {"display": "inline"}},
-        "superuser"
+        (state["value"] == "") ? "Empty" : state["value"]
     );
   }
 }
